4cbfc77a76067accc89096562f771942c221786d
marcusbierer <marcus@bierers.de>
2021-06-13 21:57:31 +0200

    GuR_GI_Main added

---
+++ GuR_GI_Main.ps1
@@ -0,0 +1,10 @@
+Write-Host "GuR Grundinstallations-Script" -ForegroundColor Green
+If(!(test-path "$PSScriptRoot\GuR_GI_Client.ps1"))
+	{
+    Import-Module BitsTransfer
+    Write-Host "Checking for Updates..."
+    Start-BitsTransfer -Source https://static.gur.de/GIScripts/GuR_GI_Client.ps1 -Destination $PSScriptRoot\GuR_GI_Client.ps1
+    }
+Start-Process -FilePath "$PSHOME\powershell.exe" -ArgumentList '-NoExit', '-File', """$PSScriptRoot\GuR_GI_Client.ps1""" –verb runAs
+Write-Host "Done" -ForegroundColor Green -NoNewline;
+Start-Sleep -s 2
