fa23b887bcc20f09965cbb7b53a4649bc3b62f71
marcus.bierer <marcus@bierers.de>
2021-06-14 20:23:55 +0200

    cleanup

--- GuR_GI_Main.ps1
+++ GuR_GI_Main.ps1
@@ -1,8 +1,9 @@
 Write-Host "GuR Grundinstallations-Script" -ForegroundColor Green
-$url_client = "https://static.gur.de/GIScripts/GuR_GI_Client.ps1"
-$output_client = "$PSScriptRoot\GuR_GI_Client.ps1"
+$url_git = "https://static.gur.de/GIScripts/dvs.zip"
+$output_git = "dvs.zip"
 Import-Module BitsTransfer
 Write-Host "Checking for Updates..."
-Start-BitsTransfer -Source $url_client -Destination $output_client | out-null
-Start-Process -FilePath "$PSHOME\powershell.exe" -ArgumentList '-NoExit', '-File', """$PSScriptRoot\GuR_GI_Client.ps1""" –verb runAs
-Write-Host "Done" -ForegroundColor Green -NoNewline;
+Start-BitsTransfer -Source $url_git -Destination $output_git | out-null
+Expand-Archive -LiteralPath dvs.zip -DestinationPath $PSScriptRoot\ -Force
+#Start-Process -FilePath "$PSHOME\powershell.exe" -ArgumentList '-NoExit', '-File', """$PSScriptRoot\GuR_GI_Client.ps1""" -verb runAs
+#Write-Host "Done" -ForegroundColor Green -NoNewline;
