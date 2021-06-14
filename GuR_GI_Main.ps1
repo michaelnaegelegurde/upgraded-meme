547591d5f249a0ae422bead64174bdd93b2883ba
marcus.bierer <marcus@bierers.de>
2021-06-14 21:55:14 +0200

    update in Main

--- GuR_GI_Main.ps1
+++ GuR_GI_Main.ps1
@@ -2,5 +2,17 @@ Write-Host "Starting Client Verison..."
 If((test-path "$PSScriptRoot\GuR_GI_Update.ps1")) {
     Move-Item -Path $PSScriptRoot\GuR_GI_Update.ps1 -Destination ..\GuR_GI_Update.ps1 -Force | out-null
 }
+function checkForUpdates {
+	Write-Host "Script auf Updates prüfen"
+    & "$PSScriptRoot\git\cmd\git.exe" fetch origin master
+    & "$PSScriptRoot\git\cmd\git.exe" reset --hard
+    #& "$PSScriptRoot\git\cmd\git.exe" pull
+    If((test-path "$PSScriptRoot\GuR_GI_Update.ps1")) {
+        Move-Item -Path $PSScriptRoot\GuR_GI_Update.ps1 -Destination ..\GuR_GI_Update.ps1 -Force | out-null
+    }
+    Write-Host "checkForUpdates done"
+    Read-Host "unicode:"
+}
+checkForUpdates
 Start-Process -FilePath "$PSHOME\powershell.exe" -ArgumentList '-NoExit', '-File', """$PSScriptRoot\GuR_GI_Client.ps1""" -verb runAs
 Write-Host "Done..." -foregroundColor green
