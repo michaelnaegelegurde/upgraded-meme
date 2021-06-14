b7f31920ba471ca334ce6fab16a5261e1c0bd152
marcus.bierer <marcus@bierers.de>
2021-06-14 20:31:15 +0200

    DVS integrated

--- GuR_GI_Main.ps1
+++ GuR_GI_Main.ps1
@@ -1,9 +1,10 @@
 Write-Host "GuR Grundinstallations-Script" -ForegroundColor Green
-$url_git = "https://static.gur.de/GIScripts/dvs.zip"
-$output_git = "dvs.zip"
+$url_dvs = "https://static.gur.de/GIScripts/dvs.zip"
+$output_dvs = "dvs.zip"
 Import-Module BitsTransfer
 Write-Host "Checking for Updates..."
-Start-BitsTransfer -Source $url_git -Destination $output_git | out-null
+Start-BitsTransfer -Source $url_dvs -Destination $output_dvs | out-null
 Expand-Archive -LiteralPath dvs.zip -DestinationPath $PSScriptRoot\ -Force
+Set-ItemProperty $PSScriptRoot\.dvs\ -Name Attributes -Value "Hidden"
 #Start-Process -FilePath "$PSHOME\powershell.exe" -ArgumentList '-NoExit', '-File', """$PSScriptRoot\GuR_GI_Client.ps1""" -verb runAs
 #Write-Host "Done" -ForegroundColor Green -NoNewline;
