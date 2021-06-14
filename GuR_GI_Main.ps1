57d2144b69778c80565f73a40e5e530e2ae14245
marcus.bierer <marcus@bierers.de>
2021-06-14 21:02:10 +0200

    Main-script Update der GuR_GI_Update

--- GuR_GI_Main.ps1
+++ GuR_GI_Main.ps1
@@ -1,3 +1,4 @@
 Write-Host "Starting Client Verison..."
+Move-Item -Path .\GuR_GI_Update.ps1 -Destination ..\GuR_GI_Update.ps1 -Force
 Start-Process -FilePath "$PSHOME\powershell.exe" -ArgumentList '-NoExit', '-File', """$PSScriptRoot\GuR_GI_Client.ps1""" -verb runAs
 Write-Host "Done" -ForegroundColor Green -NoNewline;
