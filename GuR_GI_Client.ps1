81ab5b5b368e46babc2d20f4852d4c740a605455
marcus.bierer <marcus@bierers.de>
2021-06-18 09:57:34 +0200

    UTF8 BOM

--- GuR_GI_Client.ps1
+++ GuR_GI_Client.ps1
@@ -1,9 +1,9 @@
-$PSDefaultParameterValues['Out-File:Encoding'] = 'windows1252'
+Ôªø$PSDefaultParameterValues['Out-File:Encoding'] = 'windows1252'
 Clear-Host
 Set-Location -Path $PSScriptRoot
 function checkForUpdates($Silent) {
 
-	Write-Host "Script auf Updates prùfen"
+	Write-Host "Script auf Updates prÔøΩfen"
     & "$PSScriptRoot\git\cmd\git.exe" fetch origin main
     & "$PSScriptRoot\git\cmd\git.exe" reset --hard
     & "$PSScriptRoot\git\cmd\git.exe" pull
@@ -149,7 +149,7 @@ function GuRGIMenue
     If(!(test-path "$PSScriptRoot\filedepot\ODT.exe")) {
     Write-Host "!ACHTUNG! !ACHTUNG! !ACHTUNG! !ACHTUNG!" -ForegroundColor Red
     Write-Host "Es wurden noch keine Installations-Dateien heruntergeladen!" -ForegroundColor Red
-    Write-Host "Bitte Punkt 99 ausfùhren!" -ForegroundColor Red
+    Write-Host "Bitte Punkt 99 ausfÔøΩhren!" -ForegroundColor Red
     }
 	Write-Host "GuR Grundinstallations-Script Clients $version" -ForegroundColor Green 
 	Write-Host "================================"
@@ -169,12 +169,12 @@ function GuRGIMenue
 	Write-Host
 	Write-Host "6: Office entfernen"
 	Write-Host
-	Write-Host "7: Grund-Setup (1-3 automatisch durchfùhren"
+	Write-Host "7: Grund-Setup (1-3 automatisch durchfÔøΩhren"
 	Write-Host	
     Write-Host	
 	Write-Host "96: Versionshistorie"
 	Write-Host 
-	Write-Host "97: Script auf Updates prùfen"
+	Write-Host "97: Script auf Updates prÔøΩfen"
 	Write-Host
 	Write-Host "98: Verzeichnisse bereinigen"
 	Write-Host 
@@ -234,7 +234,7 @@ switch ($gieingabe)
 	Clear-Host
    }
    '7' {
-    Write-Host "Grund-Setup (1-3 automatisch durchf¸hren)"
+    Write-Host "Grund-Setup (1-3 automatisch durchf√ºhren)"
     stdInstall "1"
     toolsCopy "1"
     regKeys "1"
